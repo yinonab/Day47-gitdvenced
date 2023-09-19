e611872a71410eb1eb3b5713ea9f0de1a93b8e78
yinonab <yinonabarjel@gmail.com>
2023-09-19 21:13:09 +0300

    F/ add last name to contact details

--- cmps/ContactDetails.jsx
+++ cmps/ContactDetails.jsx
@@ -26,7 +26,8 @@ export function ContactDetails(){
     if (!contact) return <div>Loading...</div>
     return (
         <section className="contact-details">
-            <h1 className="contact-details-title">Name: {contact.firstName}</h1>
+            <h1 className="contact-details-title">First Name: {contact.firstName}</h1>
+            <h1 className="contact-details-title">Last Name: {contact.lastName}</h1>
             <h5 className="contact-details-phone">Phone: {contact.phone}</h5>
             <p className="contact-details-icon">📝</p>
             <p className="contact-details-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi voluptas cumque tempore, aperiam sed dolorum rem! Nemo quidem, placeat perferendis tempora aspernatur sit, explicabo veritatis corrupti perspiciatis repellat, enim quibusdam!</p>
