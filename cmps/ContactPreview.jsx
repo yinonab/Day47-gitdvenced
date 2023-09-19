2bded7e065450af05412e3f230ed2892198e7f45
Ozhodeda <oz36002@gmail.com>
2023-09-19 19:47:36 +0300

    F/style-css/add style for list, preview

--- cmps/ContactPreview.jsx
+++ cmps/ContactPreview.jsx
@@ -1,11 +1,10 @@
 export function ContactPreview({onRemoveContact,onEditContact,contact}){
     return (
-        <article>
-
+        <article className="contact-article">
             <h1>Contact</h1>
-            <p><strong>{contact.firstName+' '+contact.lastName} </strong></p>
-            <p><strong>{contact.email} </strong></p>
-            <p><strong>{contact.phone} </strong></p>  
+            <p className="contact-info"><strong>{contact.firstName+' '+contact.lastName} </strong></p>
+            <p className="contact-info"><strong>{contact.email} </strong></p>
+            <p className="contact-info"><strong>{contact.phone} </strong></p>  
             <div className="contact-buttons">
                 <button className="remove-button" onClick={() => onRemoveContact(contact._id)}>
                     <i className="fa-regular fa-trash-can"></i>
