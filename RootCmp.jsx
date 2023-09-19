39ba0c07c76c5ebfd989d56ac04fa26cdab0c522
yinonab <yinonabarjel@gmail.com>
2023-09-19 14:56:22 +0300

    finish starter

---
+++ RootCmp.jsx
@@ -0,0 +1,31 @@
+ import {ContactIndex} from '../../'
+
+
+ const Router = ReactRouterDOM.HashRouter
+const { Route, Routes } = ReactRouterDOM
+const { Provider } = ReactRedux
+
+
+
+export function App() {
+
+    return (
+        <Provider store={store}>
+            <Router>
+                <section className="main-layout app">
+                    <AppHeader />
+                    <main>
+                        <Routes>
+                            {/* <Route element={<HomePage />} path="/" />
+                            <Route element={<AboutUs />} path="/about" /> */}
+                            <Route element={<TodoIndex />} path="/" />
+                        </Routes>
+                    </main>
+                    {/* <AppFooter /> */}
+                </section>
+            </Router>
+        </Provider>
+    )
+       
+    
+}
