a215f5139296cbd36012f8f8676559d3c5c34eac
Ozhodeda <oz36002@gmail.com>
2023-09-20 11:43:33 +0300

    F/create-details-cmp/add animate for Loading

--- cmps/ContactDetails.jsx
+++ cmps/ContactDetails.jsx
@@ -4,26 +4,31 @@ import { showErrorMsg } from "../services/event-bus.service.js"
 const { useEffect, useState } = React
 const { useParams, useNavigate, Link } = ReactRouterDOM
 
-export function ContactDetails(){
+export function ContactDetails() {
     const [contact, setContact] = useState(null)
-    const {contactId} = useParams()
+    const { contactId } = useParams()
     const navigate = useNavigate()
 
-    useEffect(()=>{
+    useEffect(() => {
         loadContact()
-    },[contactId])
+    }, [contactId])
 
-    function loadContact(){
+    function loadContact() {
         contactService.getById(contactId)
-        .then((contact) => setContact(contact))
-        .catch((err)=>{
-            console.log('Had issues in contact details', err)
-            showErrorMsg('Cannot load contact')
-            navigate('/contact')
-        })
+            .then((contact) => setContact(contact))
+            .catch((err) => {
+                console.log('Had issues in contact details', err)
+                showErrorMsg('Cannot load contact')
+                navigate('/contact')
+            })
     }
 
-    if (!contact) return <div>Loading...</div>
+    if (!contact) return <div className="centered-container">
+        <div className="lds-ripple">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
     return (
         <section className="contact-details">
             <h1 className="contact-details-title">First Name: <span>{contact.firstName}</span></h1>
